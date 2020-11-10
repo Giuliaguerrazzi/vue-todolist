fe306dc7a84e0267848c066d85cbd5e2cf28d3f9
Giulia <giulia.guerrazzi@live.it>
2020-11-10 17:39:22 +0100

    push newlist

--- main.js
+++ main.js
@@ -2,10 +2,17 @@
 const app = new Vue({
     el: '#app',
     data: {
+        newTodo: '',
         list: [
             'Fare la spesa',
             'Fare i compiti',
             'Comprare il pane',
         ]
+    },
+    methods: {
+        addNewTodo() {
+            this.list.push(this.newTodo)
+        },
     }
+
 });

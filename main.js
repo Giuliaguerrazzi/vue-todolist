edbdd495b0839b3ca0406713a37e959d8fabfde4
Giulia <giulia.guerrazzi@live.it>
2020-11-10 16:44:25 +0100

    add list

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        list: [
+            'Fare la spesa',
+            'Fare i compiti',
+            'Comprare il pane',
+        ]
+    }
+});
